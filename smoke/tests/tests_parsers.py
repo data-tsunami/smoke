64ae7843d64defd275374cde1acd8a4987599e9d
Horacio G. de Oro <hgdeoro@gmail.com>
2014-09-13 00:30:24 -0300

    Refactor tests into differents methods

--- smoke/tests/tests_parsers.py
+++ smoke/tests/tests_parsers.py
@@ -7,6 +7,7 @@ import uuid
 from django.test import TestCase
 from smoke.services.parsers import ApplicationMasterLaunchedParser, \
     TaskFinishedWithProgressParser, MessageFromShellParser
+import pprint
 
 GENERAL_INVALID_LINES = (
 
@@ -25,14 +26,17 @@ GENERAL_INVALID_LINES = (
 
 class MessageServiceMock(object):
 
+    def __init__(self):
+        self.messages = []
+
     def publish_message(self, line, **kwargs):
-        pass
+        self.messages.append((line, kwargs))
 
     def log_and_publish(self, message, *args, **kwargs):
-        pass
+        self.messages.append((message, args, kwargs))
 
     def log_and_publish_error(self, message, *args, **kwargs):
-        pass
+        self.messages.append((message, args, kwargs))
 
     def get_log(self):
         pass
@@ -42,7 +46,8 @@ class TestApplicationMasterLaunchedParser(TestCase):
 
     def test(self):
         cookie = uuid.uuid4().hex
-        parser = ApplicationMasterLaunchedParser(MessageServiceMock(), cookie)
+        msg_service = MessageServiceMock()
+        parser = ApplicationMasterLaunchedParser(msg_service, cookie)
 
         # TODO: get real line from real execution to test this
         LINE = ("xxx xxx INFO yarn.Client: Command for "
@@ -53,12 +58,20 @@ class TestApplicationMasterLaunchedParser(TestCase):
 
         self.assertTrue(parser.parse(LINE))
 
+        appMasterLaunched = [item.get('appMasterLaunched', False)
+                             for sublist in msg_service.messages
+                             for item in sublist
+                             if isinstance(item, dict)]
+
+        self.assertIn(True, appMasterLaunched)
+
 
 class TestTaskFinishedWithProgressParser(TestCase):
 
     def test(self):
         cookie = uuid.uuid4().hex
-        parser = TaskFinishedWithProgressParser(MessageServiceMock(), cookie)
+        msg_service = MessageServiceMock()
+        parser = TaskFinishedWithProgressParser(msg_service, cookie)
 
         LINE = ("14/08/23 12:48:53 INFO "
                 "scheduler.TaskSetManager: "
@@ -71,25 +84,20 @@ class TestTaskFinishedWithProgressParser(TestCase):
 
         self.assertTrue(parser.parse(LINE))
 
+        progressUpdate = [item.get('progressUpdate', False)
+                          for sublist in msg_service.messages
+                          for item in sublist
+                          if isinstance(item, dict)]
+
+        self.assertIn(True, progressUpdate)
+
 
 class TestMessageFromShellParser(TestCase):
 
-    def test(self):
+    def test_invalid_lines(self):
         cookie = uuid.uuid4().hex
-        wrong_cookie = uuid.uuid4().hex
-        parser = MessageFromShellParser(MessageServiceMock(), cookie)
-
-        LINE = ("@@"
-                "<msgFromShell cookie='{0}'>"
-                "<errorLine>This is the line</errorLine>"
-                "</msgFromShell>"
-                "@@".format(cookie))
-
-        WRONG_COOKIE = ("@@"
-                        "<msgFromShell cookie='{0}'>"
-                        "<errorLine>This is the line</errorLine>"
-                        "</msgFromShell>"
-                        "@@".format(wrong_cookie))
+        msg_service = MessageServiceMock()
+        parser = MessageFromShellParser(msg_service, cookie)
 
         INVALID_LINES = (
             "@@@@",
@@ -97,8 +105,9 @@ class TestMessageFromShellParser(TestCase):
             "@@<some_xml></some_xml>@@",
             "@@<msgFromShell></msgFromShell>@@",
             "@@<msgFromShell><errorLine>ERR</errorLine></msgFromShell>@@",
-            WRONG_COOKIE
         )
+
+        # -----
 
         for invalid_line in GENERAL_INVALID_LINES:
             self.assertFalse(parser.parse(invalid_line))
@@ -106,4 +115,41 @@ class TestMessageFromShellParser(TestCase):
         for invalid_line in INVALID_LINES:
             self.assertFalse(parser.parse(invalid_line))
 
+    def test_wrong_cookie(self):
+        cookie = uuid.uuid4().hex
+        wrong_cookie = uuid.uuid4().hex
+        msg_service = MessageServiceMock()
+        parser = MessageFromShellParser(msg_service, cookie)
+
+        WRONG_COOKIE = ("@@"
+                        "<msgFromShell cookie='{0}'>"
+                        "<errorLine>This is the line</errorLine>"
+                        "</msgFromShell>"
+                        "@@".format(wrong_cookie))
+
+        # -----
+
+        self.assertFalse(parser.parse(WRONG_COOKIE))
+
+    def test_errorLine(self):
+        cookie = uuid.uuid4().hex
+        msg_service = MessageServiceMock()
+        parser = MessageFromShellParser(msg_service, cookie)
+
+        error_msg = "This-is-the-line-" + uuid.uuid4().hex
+        LINE = ("@@"
+                "<msgFromShell cookie='{0}'>"
+                "<errorLine>{1}</errorLine>"
+                "</msgFromShell>"
+                "@@".format(cookie,
+                            error_msg))
+
+        # -----
+
         self.assertTrue(parser.parse(LINE))
+
+        error_msg_found = [error_msg == item
+                           for sublist in msg_service.messages
+                           for item in sublist]
+
+        self.assertIn(True, error_msg_found)
